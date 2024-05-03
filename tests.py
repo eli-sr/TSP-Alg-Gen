0006b54a39a9e18a5b668412298f0f0859a3a6c5
eli <elzr.sr@gmail.com>
2024-05-03 17:57:57 +0200

    test: Test de cruce_parcialmente_mapeado

--- tests.py
+++ tests.py
@@ -30,6 +30,28 @@ class Tests(unittest.TestCase):
         hijo = cruce_parcialmente_mapeado(padre1, padre2) 
         assert np.array_equal(hijo.value, hijoSol.value)
 
+        # El hijo devuelto es el correcto
+        mock_randint.side_effect = [0, 8]
+        padre1 = Cromosoma(13,[0,1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19])
+        padre2 = Cromosoma(13,[19,7,4,14,3,8,16,9,10,17,6,0,11,15,12,2,5,18,1])
+        hijoSol = Cromosoma(13,[0,1,2,3,4,5,6,7,8,17,16,19,11,15,12,14,10,18,9])
+        hijo = cruce_parcialmente_mapeado(padre1, padre2) 
+        assert np.array_equal(hijo.value, hijoSol.value)
+
+        # El hijo devuelto es el correcto
+        mock_randint.side_effect = [3, 6]
+        padre1 = Cromosoma(13,[1,2,3,4,5,6,7,8,9])
+        padre2 = Cromosoma(13,[4,5,3,1,8,7,6,9,2])
+        hijoSol1 = Cromosoma(13,[1,8,3,4,5,6,7,9,2])
+        hijo1 = cruce_parcialmente_mapeado(padre1, padre2) 
+        assert np.array_equal(hijo1.value, hijoSol1.value)
+
+        # El hijo devuelto es el correcto
+        mock_randint.side_effect = [3, 6]
+        hijoSol2 = Cromosoma(13,[4,2,3,1,8,7,6,5,9])
+        hijo2 = cruce_parcialmente_mapeado(padre2, padre1) 
+        assert np.array_equal(hijo2.value, hijoSol2.value)
+
 
 if __name__ == '__main__':
     unittest.main()
